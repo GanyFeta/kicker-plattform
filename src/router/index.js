398b0e81f7eebd2c38f848539e33f91292952844
atef <mail@aamri.de>
2021-09-22 14:15:44 +0200

    edit router

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,15 @@ const routes = [
     component: Home,
   },
   {
+    path: "/startGame",
+    name: "start-game",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "profil" */ "../views/StartGame.vue"),
+  },
+  {
     path: "/about",
     name: "About",
     // route level code-splitting
